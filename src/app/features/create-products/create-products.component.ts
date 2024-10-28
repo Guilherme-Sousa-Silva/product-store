c49529fe86886dfe025771ac1be7ae34a8990519
Guilherme de Sousa Silva <guilherme.sousa@interplayers.com.br>
2024-10-28 14:44:24 -0300

    feat: estilização da tela de criação de produtos

--- src/app/features/create-products/create-products.component.ts
+++ src/app/features/create-products/create-products.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
 
 @Component({
   selector: 'app-create-products',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './create-products.component.html',
   styleUrl: './create-products.component.scss'
 })
