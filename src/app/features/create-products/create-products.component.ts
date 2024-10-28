54c5d27ca90426dc3dcfd8514d8591ebf500e0d4
Guilherme de Sousa Silva <guilherme.sousa@interplayers.com.br>
2024-10-28 15:42:08 -0300

    feat: utilização do modulo de snack bar para retornar feedback ao usuario e função para retornar para a listagem após incluir produto com sucesso

--- src/app/features/create-products/create-products.component.ts
+++ src/app/features/create-products/create-products.component.ts
@@ -4,12 +4,18 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
 import { ProductsService } from '../../shared/services/products.service';
-import { CreateProduct } from '../../shared/Interfaces/create-product-interface';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-products',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [
+    ReactiveFormsModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatButtonModule,
+    MatSnackBarModule],
   templateUrl: './create-products.component.html',
   styleUrl: './create-products.component.scss'
 })
@@ -23,23 +29,36 @@ export class CreateProductsComponent {
   });
 
   constructor(
-    private productService: ProductsService
+    private productService: ProductsService,
+    private snackBar: MatSnackBar,
+    private router: Router
   ) {}
 
   onSubmit() {
     const { title } = this.form.value;
 
     if (!this.isNullOrEmpty(title)) {
+      this.showMessage('Título do produto não pode ser vazio!')
       return;
     }
+
     this.productService.create({
       title: this.form.controls.title.value
     }).subscribe(() => {
-      alert("Produto criado com sucesso");
+      this.showMessage('Produto criado com sucesso!');
+      this.router.navigateByUrl('');
     });
   }
 
   isNullOrEmpty(value: any): boolean {
     return value !== null && value !== '';
+  }
+
+  showMessage(message: string){
+    this.snackBar.open(message, 'Ok', {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    })
   }
 }
