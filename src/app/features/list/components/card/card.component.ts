e1d7dfbbbe7a0c7e314df3f71dfb05ef7030b5c2
Guilherme de Sousa Silva <guilherme.sousa@interplayers.com.br>
2024-10-30 14:54:49 -0300

    feat: função para redirecionar para a tela de edição e estilização para dar um respiro entre os cards de produtos

--- src/app/features/list/components/card/card.component.ts
+++ src/app/features/list/components/card/card.component.ts
@@ -2,6 +2,7 @@ import { Component, computed, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { Product } from '../../../../shared/Interfaces/product.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -13,4 +14,11 @@ import { Product } from '../../../../shared/Interfaces/product.interface';
 export class CardComponent {
   product = input.required<Product>();
   productTitle = computed(() => this.product().title);
+  productId = computed(() => this.product().id);
+
+  constructor(private router: Router) {}
+
+  public onEdit(id: any) {
+    this.router.navigateByUrl(`/edit-product/${id}`);
+  }
 }
